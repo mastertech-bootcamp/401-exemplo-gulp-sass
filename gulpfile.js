23824137993dddeb108fa9c8b18618125a6007d9
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-29 15:04:15 -0200

    Exemplo da aula

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('teste', function(){
+    console.log('OIEEEEEE');
+});
+ 
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('default', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
